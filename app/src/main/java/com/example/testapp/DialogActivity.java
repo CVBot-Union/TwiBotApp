166fca74ac2e1e48410e59beb65a342757501b31
Cesium <youthleaderzhj@gmail.com>
2020-08-12 05:33:15 +0800

    v1.0.1

--- app/src/main/java/com/example/testapp/DialogActivity.java
+++ app/src/main/java/com/example/testapp/DialogActivity.java
@@ -1,31 +1,97 @@
 package com.example.testapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class DialogActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+public class DialogActivity extends AppCompatActivity {
+    private String twiContent, videoPath;
+    private String[] imgPaths;
+    public static Map<String, Bitmap> cachedBmp = new HashMap<>();
+    private ViewPager2 viewPager2;
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
         this.setTitle("推文内容");
         Intent intent = getIntent();
-        String twiContent = intent.getStringExtra("twi_content");
-        String img = intent.getStringExtra("img");
+        twiContent = intent.getStringExtra("twi_content");
+        imgPaths = intent.getStringArrayExtra("img_paths");
+        videoPath = intent.getStringExtra("video_path");
         final TextView textView = findViewById(R.id.twi_content);
         textView.setText(twiContent);
-        ImageView imageView = findViewById(R.id.twi_img);
-        imageView.setImageResource(R.mipmap.ic_launcher_round);
+        if(!Arrays.equals(imgPaths, new String[]{""})) {
+            viewPager2 = findViewById(R.id.viewpager2);
+            viewPager2.setVisibility(View.VISIBLE);
+
+            viewPager2.setAdapter(new MyAdapter());
+            TextView textView1 = findViewById(R.id.img_page);
+            textView1.setVisibility(View.VISIBLE);
+            textView1.setText("共"+imgPaths.length+"页");
+        }
+        webView = findViewById(R.id.twi_video);
+        if(!videoPath.equals("")) {
+            webView.setVisibility(View.VISIBLE);
+            WebSettings setting = webView.getSettings();
+            setting.setJavaScriptEnabled(true);
+            setting.setUseWideViewPort(true);
+            setting.setLoadWithOverviewMode(true);
+            setting.setAllowFileAccess(true);
+            setting.setSupportZoom(true);
+            setting.setJavaScriptCanOpenWindowsAutomatically(true);
+            setting.setMediaPlaybackRequiresUserGesture(true);
+            webView.setWebChromeClient(new WebChromeClient());
+            webView.loadUrl(videoPath);
+            System.out.println(webView.getContentHeight());
+        }
         Button copy_button = findViewById(R.id.copy_button);
         copy_button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -36,5 +102,181 @@ public class DialogActivity extends AppCompatActivity {
                 Toast.makeText(DialogActivity.this,"复制成功",Toast.LENGTH_SHORT).show();
             }
         });
+        Button download_button = findViewById(R.id.download_media);
+        download_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                for(int i=0;i<imgPaths.length;i++) if(!imgPaths[i].equals("")) {
+                    String imgPath = imgPaths[i];
+                    String scheme = imgPath.split(":")[0];
+                    switch(scheme) {
+                        case "file":
+                            Toast.makeText(DialogActivity.this, "文件已存在于\n"+imgPath, Toast.LENGTH_LONG).show();
+                            break;
+                        case "http":
+                        case "https":
+                            Bitmap bitmap = cachedBmp.get(imgPath);
+                            Toast.makeText(DialogActivity.this, "图片"+(i+1)+"保存"+saveBitmap2file(bitmap, i), Toast.LENGTH_SHORT).show();
+                            break;
+                        default:
+                            Toast.makeText(DialogActivity.this, "图片URI格式错误", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                if(!videoPath.equals("")) {
+                    String scheme = videoPath.split(":")[0];
+                    switch(scheme) {
+                        case "file":
+                            Toast.makeText(DialogActivity.this, "文件已存在于\n"+videoPath, Toast.LENGTH_LONG).show();
+                            break;
+                        case "http":
+                        case "https":
+                            String[] tmp = videoPath.split("/");
+                            String fileName = tmp[tmp.length-1];
+                            File file1 = new File(Environment.getExternalStorageDirectory().getPath()+"/DCIM/TwiBot/"+fileName);
+                            if(!file1.exists()) {
+                                Toast.makeText(DialogActivity.this, "正在下载视频", Toast.LENGTH_SHORT).show();
+                                downloadVideo(videoPath, fileName);
+                            }
+                            else Toast.makeText(DialogActivity.this,"文件已存在于\n"+file1.getPath(), Toast.LENGTH_SHORT).show();
+                            break;
+                        default:
+                            Toast.makeText(DialogActivity.this, "视频URI格式错误", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                //Log.d("DialogActivity","下载");
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(webView != null) {
+            webView.stopLoading();
+            webView.clearHistory();
+            webView.clearCache(true);
+            webView.loadUrl("about:blank"); // clearView() should be changed to loadUrl("about:blank"), since clearView() is deprecated now
+            webView.pauseTimers();
+            webView.destroy(); // Note that mWebView.destroy() and mWebView = null do the exact same thing
+        }
+    }
+
+    public Bitmap getURLImage(final String url) {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        Future<Bitmap> future = pool.submit(new Callable<Bitmap>() {
+            @Override
+            public Bitmap call() {
+                Bitmap bmp = null;
+                try {
+                    URL myUrl = new URL(url);
+                    // 获得连接
+                    HttpURLConnection conn = (HttpURLConnection) myUrl.openConnection();
+                    conn.setConnectTimeout(6000);//设置超时
+                    conn.setDoInput(true);
+                    conn.setUseCaches(false);//不缓存
+                    conn.connect();
+                    InputStream is = conn.getInputStream();//获得图片的数据流
+                    bmp = BitmapFactory.decodeStream(is);//读取图像数据
+                    //读取文本数据
+                    //byte[] buffer = new byte[100];
+                    //inputStream.read(buffer);
+                    //text = new String(buffer);
+                    is.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return bmp;
+        }});
+        try {
+            return future.get();
+        } catch(ExecutionException|InterruptedException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String saveBitmap2file(Bitmap bmp, int index) {
+        String[] tmp = imgPaths[index].split("/");
+        String fileName = tmp[tmp.length-1].split("\\.")[0];
+        try {
+            String filePath = Environment.getExternalStorageDirectory().getPath() + "/DCIM/TwiBot/" + fileName + ".jpeg";
+            File file = new File(filePath);
+            if(!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            FileOutputStream fos = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.JPEG,100, fos);
+            fos.flush();
+            fos.close();
+            DialogActivity.this.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + filePath)));
+            this.imgPaths[index] = "file://" + filePath;
+            return "成功";
+        } catch(Exception e){
+            e.printStackTrace();
+            return "失败";
+        }
+    }
+
+    public void downloadVideo(final String url, final String fileName) {
+        try {
+            DownloadManager downloadManager = (DownloadManager)getSystemService(DOWNLOAD_SERVICE);
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            request.allowScanningByMediaScanner();
+            request.setVisibleInDownloadsUi(true);
+            request.setTitle("转推视频下载");
+            request.setDestinationInExternalPublicDir("/DCIM/TwiBot/", fileName);
+            downloadManager.enqueue(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(DialogActivity.this, "视频下载失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView imageView;
+
+            public ViewHolder(View view) {
+                super(view);
+                imageView = view.findViewById(R.id.twi_img);
+            }
+        }
+
+        public MyAdapter() {}
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.img_item, parent,false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            String imgPath = imgPaths[position];
+            if(!imgPath.equals("")) {
+                holder.imageView.setVisibility(View.VISIBLE);
+                String scheme = imgPath.split(":")[0];
+                switch(scheme) {
+                    case "file":
+                        holder.imageView.setImageURI(Uri.parse(imgPath));
+                        break;
+                    case "http":
+                    case "https":
+                        if(!cachedBmp.containsKey(imgPath)) cachedBmp.put(imgPath,getURLImage(imgPath));
+                        holder.imageView.setImageBitmap(cachedBmp.get(imgPath));
+                        break;
+                    default:
+                        Log.e("DialogActivity", "ImageURI格式错误");
+                }
+            }
+        }
+        @Override
+        public int getItemCount() {
+            return imgPaths.length;
+        }
     }
 }
--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.testapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -8,42 +13,64 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.litepal.LitePal;
+import org.litepal.annotation.Column;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    private String[] data = {"apple","bannner","orange","watermelon","pear","grape",
-            "pineapple","strawberry","cherry","mango","apple","bannner","orange",
-            "watermelon","pear","grape", "pineapple","strawberry","cherry","mango"
-    };
+    private List<String> twiContents = new ArrayList<>(Arrays.asList("apple", "bannner", "orange", "watermelon", "pear", "grape",
+            "pineapple", "strawberry", "cherry", "mango", "apple", "bannner", "orange",
+            "watermelon", "pear", "grape", "pineapple", "strawberry", "cherry", "mango"));
+    private List<String[]> imgPathsList = new ArrayList<>(Arrays.asList(
+            new String[]{"https://i0.hdslb.com/bfs/archive/22650682fd25a4a5aa96dd9ef53190c6b8d54912.png"},
+            new String[]{"https://himg.bdimg.com/sys/portrait/item/5815b7e7b7c9d8df9020.jpg", "https://upload-images.jianshu.io/upload_images/677256-1fb6afa3593d1b2c.jpg"},
+            new String[]{""}));
+    private List<String> videoPathList = new ArrayList<>(Arrays.asList(
+            "http://101.200.184.98:8080/media/ms7Igd7pov9gBxfy.mp4", "", "http://101.200.184.98:8080/media/7v1nYn8AjwwP-O4B.mp4"));
     private String s = "斎藤ニコル\n08-05 18:02:28\n\u6700\u8fd1\u3059\u3063\u3054\u304f\u6691\u3044\u306d(>_<)\n\n\u79c1\u306f\u4eca\u65e5\u30b9\u30a4\u30ab\u3092\u98df\u3079\u305f\u3088\ud83c\udf49\n\u307f\u306a\u3055\u3093\u3082\u590f\u3063\u307d\u3044\u3053\u3068\u4f55\u304b\u3057\u307e\u3057\u305f\u304b\u30fc\uff1f\ud83d\udc93";
     private NetworkChangeReceiver networkChangeReceiver;
+    private SwipeRefreshLayout refreshLayout;
+    private TwiAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.setTitle("Home");
+        if(twiContents.size()!=imgPathsList.size()||twiContents.size()!=videoPathList.size())
+            Toast.makeText(MainActivity.this,"推文与媒体列表长度不一致\n请与开发者联系", Toast.LENGTH_LONG).show();
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
-        data[0] = s;
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,data);
-        ListView listView = findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new ListView.OnItemClickListener() {
+        //LitePal.getDatabase();
+        //读取数据库中的数据
+        //queryTwiData();
+        twiContents.set(0, s);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        adapter = new TwiAdapter();
+        recyclerView.setAdapter(adapter);
+        refreshLayout = findViewById(R.id.swipe_refresh);
+        refreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent= new Intent(MainActivity.this,DialogActivity.class);
-                intent.putExtra("twi_content",data[position]);
-                intent.putExtra("img","@mipmap/ic_launcher_round");
-                startActivity(intent);
+            public void onRefresh() {
+                netRefresh();
             }
         });
     }
@@ -51,8 +78,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        DialogActivity.cachedBmp.clear();
         unregisterReceiver(networkChangeReceiver);
     }
+
+    public void netRefresh() {
+        RefreshTask task = new RefreshTask();
+        task.execute();
+        //更新数据库数据
+        //updateTwiData();
+    }
+
+    public void updateTwiData() {}
+
+    public void queryTwiData() {}
 
     class NetworkChangeReceiver extends BroadcastReceiver {
         @Override
@@ -60,7 +99,106 @@ public class MainActivity extends AppCompatActivity {
             ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
             assert connectivityManager != null;
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if(networkInfo == null || networkInfo.isAvailable()) Toast.makeText(context, "无网络连接", Toast.LENGTH_LONG).show();
+            if(networkInfo == null || !networkInfo.isAvailable()) Toast.makeText(context, "无网络连接", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class TwiAdapter extends RecyclerView.Adapter<TwiAdapter.ViewHolder> {
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            TextView textView;
+
+            public ViewHolder(View view) {
+                super(view);
+                textView = view.findViewById(android.R.id.text1);
+            }
+        }
+
+        public TwiAdapter() {}
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent,false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+            holder.textView.setText(twiContents.get(position));
+            holder.textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent= new Intent(MainActivity.this, DialogActivity.class);
+                    intent.putExtra("twi_content",twiContents.get(position));
+                    intent.putExtra("img_paths", imgPathsList.get(position));
+                    intent.putExtra("video_path", videoPathList.get(position));
+                    startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() { return twiContents.size(); }
+    }
+
+    public class TwiData extends LitePalSupport {
+        @Column(nullable = false)
+        private String content;
+
+        private String[] imgPaths;
+
+        @Column(defaultValue = "")
+        private String videoPath;
+
+        public TwiData(String content, String[] imgPaths, String videoPath) {
+            this.content = content;
+            this.imgPaths = imgPaths.clone();
+            this.videoPath = videoPath;
+        }
+
+        public String getContent() {return content;}
+        public void setContent(String content) {this.content = content;}
+
+        public String[] getImgPaths() {return imgPaths;}
+        public void setImgPaths(String[] imgPaths) {this.imgPaths = imgPaths.clone();}
+
+        public String getVideoPath() {return videoPath;}
+        public void setVideoPath(String videoPath) {this.videoPath = videoPath;}
+    }
+
+    class RefreshTask extends AsyncTask<String,Void,Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            refreshLayout.setRefreshing(true);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            //实际应用中，此处与服务器通信以获取数据
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+            TwiData[] newTwiData = new TwiData[]{new TwiData("新增条目", new String[]{""}, ""), new TwiData("新增条目2", new String[]{""}, "")};
+            for(TwiData singleTwi:newTwiData) {
+                twiContents.add(0, singleTwi.getContent());
+                imgPathsList.add(0, singleTwi.getImgPaths());
+                videoPathList.add(0, singleTwi.getVideoPath());
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            refreshLayout.setRefreshing(false);
+            if(result)
+                adapter.notifyDataSetChanged();
+            else
+                Toast.makeText(MainActivity.this, "刷新失败", Toast.LENGTH_SHORT).show();
         }
     }
 }
