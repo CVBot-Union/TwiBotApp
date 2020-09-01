32ffe2fec4f619ec279cb8677a5a21c59426c6c3
Xiaofan2 <xiaofan2.rs@icloud.com>
2020-09-01 21:38:25 +0800

    优化RecyclerView，改变标题栏

--- app/src/main/java/com/example/testapp/TweetList.java
+++ app/src/main/java/com/example/testapp/TweetList.java
@@ -3,6 +3,7 @@ package com.example.testapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SimpleItemAnimator;
 
 import android.os.Bundle;
 
@@ -27,7 +28,6 @@ public class TweetList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tweet_list);
 
-
         TwitterUser user = new TwitterUser("1","sb","sbsb","SB","https://pbs.twimg.com/profile_images/1206156308602163200/MO4FkO4N_400x400.jpg");
         TwitterMedia media = new TwitterMedia("1","https://pbs.twimg.com/profile_images/1206156308602163200/MO4FkO4N_400x400.jpg",TwitterMedia.IMAGE,"https://pbs.twimg.com/profile_images/1206156308602163200/MO4FkO4N_400x400.jpg");
         ArrayList<TwitterMedia> newList = new ArrayList<>();
@@ -40,15 +40,17 @@ public class TweetList extends AppCompatActivity {
         dataSet.add(status);
         dataSet.add(status);
         dataSet.add(status);
-        dataSet.add(status);
-        dataSet.add(status);
-        dataSet.add(status);
 
         tweetList = (RecyclerView) findViewById(R.id.tweet_list_recycler_view);
-        layoutManager = new LinearLayoutManager(this);
+        layoutManager = new LinearLayoutManager(this){
+            @Override
+            public boolean canScrollVertically() {
+                return false;
+            }
+        };
         tweetList.setLayoutManager(layoutManager);
-        tweetList.setHasFixedSize(true);
         tAdapter = new TweetCardAdapter(dataSet);
         tweetList.setAdapter(tAdapter);
+        ((SimpleItemAnimator) tweetList.getItemAnimator()).setSupportsChangeAnimations(false);
     }
 }
