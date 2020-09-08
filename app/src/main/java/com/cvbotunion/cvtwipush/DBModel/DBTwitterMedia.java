3cfab63e5a452ef551b317ed2cc841f72de8dee5
Cesium <youthleaderzhj@gmail.com>
2020-09-08 20:30:19 +0800

    add intialization methods for DB classes

--- app/src/main/java/com/cvbotunion/cvtwipush/DBModel/DBTwitterMedia.java
+++ app/src/main/java/com/cvbotunion/cvtwipush/DBModel/DBTwitterMedia.java
@@ -9,15 +9,22 @@ import org.litepal.crud.LitePalSupport;
  * Model class of the database, which corresponds to class TwitterMedia.
  */
 public class DBTwitterMedia extends LitePalSupport {
+    @Column(unique = true)
     private String tid;  //见DBTwitterStatus的tid注释
 
-    @Column(unique = true)
     private String url;
 
     private String previewImageURL;
 
     @Column(nullable = false)
     private int type;
+
+    public DBTwitterMedia(TwitterMedia twitterMedia) {
+        this.tid = twitterMedia.id;
+        this.url = twitterMedia.url;
+        this.previewImageURL = twitterMedia.previewImageURL;
+        this.type = twitterMedia.type;
+    }
 
     public TwitterMedia toTwitterMedia() {
         return new TwitterMedia(tid, url, type, previewImageURL);
